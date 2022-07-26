947b53e412d134727c84853a4e9aca895ed4029c
Timothy Mei <timothymei3270@gmail.com>
2022-07-26 11:05:24 -0400

    removed node modules and package-lock.json from tracking

---
+++ client/src/components/Users.jsx
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { useEffect } from "react"
+
+const BASE_URL = 'http://localhost:3001'
+
+const Users = () => {
+  const [users, setUsers] = useState([])
+
+  const getUsers = async () => {
+
+  }
+}
