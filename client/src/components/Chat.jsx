53bf12de3c34ac6962b52a3a26428b26cfc8fe71
Timothy Mei <timothymei3270@gmail.com>
2022-07-27 14:33:23 -0400

    cleaned up code and fixed receive messages

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,6 +1,4 @@
-import { useState, useEffect, ReactDOM, createRoot } from "react";
-// import { ReactDOM } from 'react-dom'
-import SelfBubble from './SelfBubble'
+import { useState, useEffect } from "react";
 import io from 'socket.io-client'
 import axios from 'axios'
 
@@ -11,6 +9,16 @@ const Chat = () => {
 const [message, setMessage] = useState('')
 const [update, setUpdate] = useState('')
 
+  const handleChange = (event) => {
+    setMessage( event.target.value );
+  };
+
+  const sendMessage = async () => {
+    socket.emit('send_message', {message: message})
+    // clear the form
+    setMessage('');
+  }
+  
 useEffect(() => {
   socket.on('receive_message', (data) => {
     //create received text bubble
@@ -18,39 +26,17 @@ useEffect(() => {
   })
 }, [socket])
 
-
-  const handleChange = (event) => {
-    setMessage( event.target.value );
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(message);
-
-    const sendMessage = async () => {
-      if (message !== ''){
-        socket.emit('send_message', {message: message})
-      }
-      sendMessage()
-      // clear the form
-      setMessage('');
-    }
-  };
-
-
-
   return (
     <div>
       <div className="user-container">
         {/* user component */}
       </div>
       <div className="chat-header">
-        {/* <SelfBubble setUpdate={setUpdate()}/> */}
       </div>
       <div className="chat-body"></div>
       <div className="chat-footer">
       <input type="text" placeholder="Message" onChange={handleChange} value={message}/>
-      <button onClick={handleSubmit}>send</button>
+      <button onClick={sendMessage}>send</button>
       input needs onChange and onSubmit
       </div>
     </div>
--- server/server.js
+++ server/server.js
@@ -23,13 +23,13 @@ const io = new Server(server, {
 io.on('connection', (socket) => {
   console.log(`User connected: ${socket.id}`)
 
+  socket.on('disconnect', () => {
+    console.log(`User Disconnected: ${socket.id}`)
+  })
+
   socket.on('send_message', (data) => {
     console.log(data)
     socket.broadcast.emit('receive_message', data)
-  })
-
-  socket.on('disconnect', () => {
-    console.log(`User Disconnected: ${socket.id}`)
   })
 })
 
