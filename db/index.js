67e24b56e3e57db1d03941001a3e6c931905852a
Timothy Mei <timothymei3270@gmail.com>
2022-07-30 12:52:25 -0400

    making adjustments to atlas/heroku

--- db/index.js
+++ db/index.js
@@ -4,12 +4,12 @@ require('dotenv').config()
 let dbUrl =
   process.env.NODE_ENV === 'production'
     ? process.env.MONGODB_URI
-    : 'mongodb://127.0.0.1:27017/textOneDatabase'
+    : `mongodb+srv://timothy:${process.env.MONGODBPASSWORD}@cluster0.yd2drxq.mongodb.net/textOneDatabase`
 
 mongoose
   .connect(dbUrl)
   .then(() => {
-    console.log('Successfully connected to MongoDB.')
+    console.log('Successfully connected to MongoDB!')
   })
   .catch((e) => {
     console.error('Connection error', e.message)
