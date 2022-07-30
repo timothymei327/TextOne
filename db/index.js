e911f89413f03991e12eb5479d7c6ce3f4aa0292
Timothy Mei <timothymei3270@gmail.com>
2022-07-30 13:21:18 -0400

    atlas/heroku

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 let dbUrl =
   process.env.NODE_ENV === 'production'
     ? process.env.MONGODB_URI
-    : 'mongodb://127.0.0.1:27017/Cluster0'
+    : 'mongodb://127.0.0.1:27017/textOneDatabase'
 
 mongoose
   .connect(dbUrl)
