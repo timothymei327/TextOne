443d06e55e4abc9e8049c0fc261cd54bcdf9ab27
Timothy Mei <timothymei3270@gmail.com>
2022-08-02 10:10:51 -0400

    added atlas and heroku connection

--- db/index.js
+++ db/index.js
@@ -1,20 +1,15 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-let dbUrl =
-  process.env.NODE_ENV === 'production'
-    ? process.env.MONGODB_URI
-    : 'mongodb://127.0.0.1:27017/textOneDatabase'
-
 mongoose
-  .connect(dbUrl)
+  .connect(process.env.MONGODB_URL)
   .then(() => {
-    console.log('Successfully connected to MongoDB!')
+    console.log('Successfully connected to MongoDB.')
   })
   .catch((e) => {
     console.error('Connection error', e.message)
   })
-mongoose.set('debug', true)
+// mongoose.set('debug', true)
 const db = mongoose.connection
 
 module.exports = db
